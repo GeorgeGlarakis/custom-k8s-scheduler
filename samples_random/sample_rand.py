9999ff607caf040a9f1976e7f90ab06aa4307692
George Glarakis <george.glarakis@gmail.com>
2024-06-21 18:13:36 +0300

    Edit scheduler

--- samples_random/sample_rand.py
+++ samples_random/sample_rand.py
@@ -7,6 +7,7 @@ import json
 from kubernetes import client, config, watch
 
 config.load_kube_config()
+# config.load_incluster_config()
 v1=client.CoreV1Api()
 
 scheduler_name = "foobar"
@@ -17,32 +18,52 @@ def nodes_available():
             for status in n.status.conditions:
                 if status.status == "True" and status.type == "Ready":
                     ready_nodes.append(n.metadata.name)
+    
+    print("--- NODES ---")
+    print(ready_nodes)
     return ready_nodes
 
-def scheduler(name, node, namespace="default"):
-    body=client.V1Binding()
-        
-    target=client.V1ObjectReference()
-    target.kind="Node"
-    target.apiVersion="v1"
-    target.name= node
+# def scheduler(name, node, namespace="default"):
+#     target=client.V1ObjectReference()
+#     target.kind="Node"
+#     target.apiVersion="v1"
+#     target.name=node
     
-    meta=client.V1ObjectMeta()
-    meta.name=name
+#     meta=client.V1ObjectMeta()
+#     meta.name=name
+
+#     body=client.V1Binding(target=target)    
+#     # body.target=target
+#     body.metadata=meta
     
-    body.target=target
-    body.metadata=meta
+#     return v1.create_namespaced_pod_binding(name, namespace, body)
+
+def scheduler(pod, node, namespace="default"):
+    # Set node to be scheduled
+    body = {
+        "spec": {
+            "nodeName": node
+        }
+    }
+
+    scheduler_status = v1.patch_namespaced_pod(name=pod.metadata.name, namespace=pod.metadata.namespace, body=body)
+
+    print("--- SCHEDULER STARUS ---")
+    print(scheduler_status)
+    return scheduler_status
     
-    return v1.create_namespaced_binding_binding(name, namespace, body)
 
 def main():
+    print("Custom Scheduler Started Successfully")
     w = watch.Watch()
+    print("Start Watching...")
     for event in w.stream(v1.list_namespaced_pod, "default"):
         if event['object'].status.phase == "Pending" and event['object'].spec.scheduler_name == scheduler_name:
+            print("Try scheduling Pod: ", event['object'].metadata.name)
             try:
-                res = scheduler(event['object'].metadata.name, random.choice(nodes_available()))
+                res = scheduler(event['object'], random.choice(nodes_available()))
             except client.rest.ApiException as e:
-                print json.loads(e.body)['message']
+                print(json.loads(e.body)['message'])
                     
 if __name__ == '__main__':
     main()
