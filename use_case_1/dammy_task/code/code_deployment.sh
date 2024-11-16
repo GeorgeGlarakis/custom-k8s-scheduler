cdfb9ad9343cc258e16b9f989eae1cf376eadea0
George Glarakis <george.glarakis@gmail.com>
2024-11-16 22:27:15 +0200

    #1 Edit Dummy Task

--- use_case_1/dammy_task/code/code_deployment.sh
+++ use_case_1/dammy_task/code/code_deployment.sh
@@ -6,27 +6,33 @@ kind: Job
 metadata:
   name: code-job
   labels:
-    app: code_1
+    app: code-1
 spec:
   template:
     metadata:
       labels:
-        app: code_1
+        app: code-1
         role: code
         complexity: simple
-        data_id: dammy_1
+        data_id: data-1
     spec:
       schedulerName: $SCHEDULER_NAME
       containers:
       - name:  code-pod
         image: docker.io/glarakis99/code_pod:latest
         env:
-        - name: NODE_NAME
-          value: $NODE_NAME
+        - name: LOG_LEVEL
+          value: $LOG_LEVEL
         - name: DATA_ID
           valueFrom:
             fieldRef:
               fieldPath: metadata.labels['data_id']
-      restartPolicy:
+        - name: NAMESPACE
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.namespace
+      restartPolicy: Never
 
 EOF
+
+# Add PodAffinity
