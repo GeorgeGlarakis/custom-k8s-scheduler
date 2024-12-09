89b6690869a7a0d358989d2488df73d70b2087dd
George Glarakis <george.glarakis@gmail.com>
2024-12-09 22:18:47 +0200

    #3 Update Postgres

--- use_case_1/master_agent/postgres/init.sql
+++ use_case_1/master_agent/postgres/init.sql
@@ -7,8 +7,7 @@ CREATE TABLE node (
     name VARCHAR(255) NOT NULL,
     cpu_speed INTEGER, -- MHz 10^6 Hz
     memory INTEGER,
-    disk_size INTEGER,
-    PRIMARY KEY (id)
+    disk_size INTEGER
 );
 
 CREATE TABLE code (
@@ -17,16 +16,14 @@ CREATE TABLE code (
     image VARCHAR(255),
     tag VARCHAR(255),
     complexity VARCHAR(255), -- O(n) | O(n^2) | O(nlogn)
-    size_mb NUMERIC(10,1) DEFAULT 0,
-    PRIMARY KEY (id)
+    size_mb NUMERIC(10,1) DEFAULT 0
 );
 
 CREATE TABLE data (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     count_n INTEGER, -- len(list)
-    size_mb NUMERIC(10,1) DEFAULT 0,
-    PRIMARY KEY (id)
+    size_mb NUMERIC(10,1) DEFAULT 0
 );
 
 CREATE TABLE task (
@@ -39,8 +36,7 @@ CREATE TABLE task (
     time_completed TIMESTAMP,
     node_id INTEGER,
     execution_prediction_ms NUMERIC(10,2),
-    completion_prediction_ms NUMERIC(10,2),
-    PRIMARY KEY (id)
+    completion_prediction_ms NUMERIC(10,2)
 );
 
 CREATE TABLE compatible (
@@ -49,20 +45,20 @@ CREATE TABLE compatible (
     PRIMARY KEY (code_id, data_id)
 );
 
+CREATE TYPE pod_type AS ENUM ('code', 'data');
+
 CREATE TABLE node_info (
     id SERIAL PRIMARY KEY,
     time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     time_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     node_id INTEGER NOT NULL,
     pod_id INTEGER NOT NULL,
-    pod_type ENUM('code', 'data') NOT NULL,
-    PRIMARY KEY (id)
+    pod_type  pod_type NOT NULL
 );
 
 CREATE TABLE node_latency (
     id SERIAL PRIMARY KEY,
     node_from INTEGER NOT NULL,
     node_to INTEGER NOT NULL,
-    latency_ms INTEGER NOT NULL DEFAULT 0,
-    PRIMARY KEY (id)
+    latency_ms INTEGER NOT NULL DEFAULT 0
 );
